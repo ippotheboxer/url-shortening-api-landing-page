03e2574100f01b140327a95afb88d4f3a341bf08
Sophie <sophiesokol123@gmail.com>
2025-02-12 14:53:04 +0000

    Add responsive boost links section

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import InputURL from "./components/InputURL";
 import Statistics from "./components/Statistics";
+import BoostLinks from "./components/BoostLinks";
 
 const App:React.FC = () => {
   return (
     <main>
       <Header />
       <Hero />
-      <div className='bg-gray/25 mt-20'>
-      <InputURL />
-      <Statistics />
+      <div className='bg-gray/25 mt-20 pb-10'>
+        <InputURL />
+        <Statistics />
       </div>
+      <BoostLinks />
     </main>
   )
 }
---
+++ src/components/BoostLinks.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+import Wrapper from './Wrapper';
+
+const BoostLinks: React.FC = () => {
+  return (
+    <div className='flex flex-col items-center text-center bg-center bg-cover bg-dark-violet bg-[url(assets/images/bg-boost-desktop.svg)]'>
+        <Wrapper>
+            <h2 className='pb-4 text-white font-poppins poppins-bold text-3xl md:text-4xl'>Boost your links today</h2>
+            <Button>Get started</Button>
+        </Wrapper>
+    </div>
+  );
+}
+
+export default BoostLinks;
