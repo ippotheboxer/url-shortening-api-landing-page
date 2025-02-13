71462019170d2b98d631d3b102308d866dac6dad
Sophie <sophiesokol123@gmail.com>
2025-02-13 20:49:09 +0000

    Improve styling of url list items

--- src/components/URLsubmission/LinkListItem.tsx
+++ src/components/URLsubmission/LinkListItem.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '../Button';
 
 interface Props {
     givenURL: string;
@@ -8,18 +7,20 @@ interface Props {
 
 const LinkListItem: React.FC<Props> = ({givenURL, shortenedURL, ...props}) => {
   return (
-    <div {...props} className='bg-white px-6 py-3 mb-4 mt-6 rounded-sm flex flex-row items-center justify-between font-poppins poppins-medium'>
-        <h4 className='text-black'>{givenURL}</h4>
-        <div className='flex flex-row items-center'>
-            <h4 className='text-cyan pr-8'>{shortenedURL}</h4>
-            <button 
-              className='bg-cyan font-poppins text-white poppins-bold py-2 px-6 rounded-sm hover:cursor-pointer'
-              onClick={() => {}}
-              >
-              Copy
-            </button>
-          </div>
-    </div>
+    <div className='mb-4 mt-6 xl:px-42 lg:px-28 md:px-10 sm:px-8 px-6'>
+      <div {...props} className='bg-white rounded-sm flex md:flex-row flex-col md:items-center justify-between font-poppins poppins-medium'>
+          <h4 className='px-6 py-3 text-black border-gray/20 border-b-2 md:border-0'>{givenURL}</h4>
+          <div className='px-6 py-3 flex md:flex-row flex-col md:items-center'>
+              <h4 className='pb-2 md:pb-0 text-cyan pr-8'>{shortenedURL}</h4>
+              <button 
+                className='bg-cyan font-poppins text-white poppins-bold py-2 px-6 rounded-sm hover:cursor-pointer w-10/12 mb-2 md:mb-0'
+                onClick={() =>  navigator.clipboard.writeText(shortenedURL)}
+                >
+                Copy
+              </button>
+            </div>
+      </div>
+      </div>
   );
 }
 
