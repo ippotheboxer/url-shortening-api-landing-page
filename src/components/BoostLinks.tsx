ee1b1a17e82ffe63229a68138cdb15e92fee7dcc
Sophie <sophiesokol123@gmail.com>
2025-02-14 13:10:38 +0000

    Change bg on boost links section when screen size is mobile

--- src/components/BoostLinks.tsx
+++ src/components/BoostLinks.tsx
@@ -4,7 +4,7 @@ import Wrapper from './Wrapper';
 
 const BoostLinks: React.FC = () => {
   return (
-    <div className='flex flex-col items-center text-center bg-center bg-cover bg-dark-violet bg-[url(assets/images/bg-boost-desktop.svg)]'>
+    <div className='flex flex-col items-center text-center bg-center bg-cover bg-dark-violet md:bg-[url(assets/images/bg-boost-desktop.svg)] bg-[url(assets/images/bg-boost-mobile.svg)]'>
         <Wrapper>
             <h2 className='text-white font-poppins poppins-bold p-8 text-3xl md:text-4xl'>Boost your links today</h2>
             <Button>Get started</Button>
